745e5d71f9d9b107195d16abb28b9165f3a2ab40
Simon Dormeau <simon.dormeau@etud.u-picardie.fr>
2017-03-18 09:43:00 +0100

    création écran titre et classe affichage

---
+++ jeu/jeu/affichage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace jeu
+{
+    internal class affichage
+    {
+        public void affiche(string text)
+        {
+            Console.WriteLine(text);
+        }
+        public void ligH(char car)
+        {
+            for(int i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.Write(car);
+            }
+        }
+        public void ligH(char car,char car2)
+        {
+            for (int i = 0; i < Console.BufferWidth/2; i++)
+            {
+                Console.Write(car);
+                Console.Write(car2);
+            }
+        }
+    }
+}
