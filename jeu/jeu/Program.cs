31e8fd97e3c11cf69e564853a8797a9cabd500f5
Simon Dormeau <simon.dormeau@etud.u-picardie.fr>
2017-03-18 10:23:14 +0100

    création menu départ +nouvelle méthode "centrage" affichage

--- jeu/jeu/Program.cs
+++ jeu/jeu/Program.cs
@@ -10,8 +10,29 @@ namespace jeu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("initial commit");
-            Console.ReadLine();
+            affichage af = new affichage();
+            Console.Title = "CMD_ adventure";   //défini le titre de la console
+
+
+            af.ligH('=');
+            af.centrage("Bienvenue Dans CMD_ Adventure !");
+            af.ligH('=');
+            af.affiche("appuyez sur une touche pour continuer...");
+            Console.ReadKey(true);  // attend la saisie d'un appui sur clavier
+            Console.Clear(); //efface la console
+            af.ligH('=');
+            af.centrage("Bienvenue Dans CMD_ Adventure !");
+            af.ligH('=');
+            af.centrage(af.perso_base);
+
+
+            Console.ReadKey(true);
         }
     }
 }
+/* Référence utiles:
+ * https://msdn.microsoft.com/fr-fr/library/ms228362.aspx#Anchor_4    les séquences d'éhapement de chaîne
+ * https://tinyurl.com/lggakbh formatage des chaines de carac.
+ * 
+ * 
+ */
