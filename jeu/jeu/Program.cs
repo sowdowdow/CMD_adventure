6a2bc0ad72df78a7fcc0f8ae87cfda70409b647e
Simon Dormeau <simon.dormeau@etud.u-picardie.fr>
2017-03-18 09:07:21 +0100

    initial commit

---
+++ jeu/jeu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("initial commit");
+            Console.ReadLine();
+        }
+    }
+}
