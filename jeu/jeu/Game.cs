f646f9b519b677a6b55cb7e865b78b9aff636bd8
sowdowdow <d.simon_97400@hotmail.fr>
2018-09-28 11:50:12 +0200

    Moved Taptaptap to a class + fixed it

--- jeu/jeu/Game.cs
+++ jeu/jeu/Game.cs
@@ -1,4 +1,5 @@
 ﻿using Graphics;
+using jeu;
 using System;
 using System.Globalization;
 using System.Text;
@@ -29,7 +30,7 @@ namespace game
             //_drawer.CrazyConsoleRandomNumber();    //<--------------------------------réactiver a la fin du dev.
             Console.ForegroundColor = _UIcolor;
             //_drawer.TitleScreen();                    //<--------------------------------réactiver a la fin du dev.
-            //Taptaptap_game();                 //<--------------------------------réactiver a la fin du dev.
+            new Taptaptap();                 //<--------------------------------réactiver a la fin du dev.
             _mutexLifeBar = true;
         }
 
@@ -52,154 +53,6 @@ namespace game
             }
         }
         #endregion timer
-
-        /**
-         * mini-game at the beginning of the party
-         * 
-         * Allow the player to get the UI
-         */
-        public void Taptaptap_game()
-        {
-            Console.Clear();
-            _drawer.HorizontalLine(0, '=');
-            Console.SetCursorPosition(0, 2);
-            ConsoleColor fg_actuel = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.White;
-            _drawer.CenterWrite("tap tap tap !");
-            Console.ForegroundColor = fg_actuel;
-            _drawer.HorizontalLine(4, '=');
-            Console.CursorVisible = false;
-
-            // display the player sprite fall
-            for (int i = Console.CursorTop + 1; i < Console.WindowHeight; i++)
-            {
-                _drawer.CenterWrite(Sprite_box._player_base);
-                // the duration is 1 sec
-                // so each line last a division by
-                // the number of lines
-                _drawer.Wait(1000 / Console.WindowHeight);
-                Console.Write("\r");
-                _drawer.CenterWrite("   ");
-                Console.Write("\n");
-            }
-            _drawer.CenterWrite(Sprite_box._player_base);
-            Console.CursorVisible = true;
-
-
-            // Mini-Game loop
-            // finish with a score of 1K
-            while (Stats.Player.TaptaptapScore < 1000)
-            {
-                _drawer.DeleteLine(3);
-                Console.SetCursorPosition(0, 3);
-                Console.ReadKey();
-                Stats.Player.TaptaptapScore++;
-                Console.SetCursorPosition(Console.WindowWidth - 10, Console.WindowHeight - 2);
-                _drawer.Write("ttt: " + Stats.Player.TaptaptapScore);
-                switch (Stats.Player.TaptaptapScore)
-                {
-                    case 10:
-                        Console.SetCursorPosition(0, 5);
-                        _drawer.DeleteLine(5);
-                        _drawer.Write("Woaw 10 tap !");
-                        break;
-                    case 50:
-                        Console.SetCursorPosition(0, 5);
-                        _drawer.DeleteLine(5);
-                        _drawer.Write("50 tap !");
-                        break;
-                    case 100:
-                        Console.SetCursorPosition(0, 5);
-                        _drawer.DeleteLine(5);
-                        _drawer.Write("100 tap !!");
-                        break;
-                    case 200:
-                        Console.SetCursorPosition(0, 5);
-                        _drawer.DeleteLine(5);
-                        _drawer.Write("100 tap !!");
-                        break;
-                    case 500:
-                        Console.SetCursorPosition(0, 5);
-                        _drawer.DeleteLine(5);
-                        _drawer.Write("500 tap !!!! some more for a gift :)");
-                        break;
-                    case 1000:
-                        Console.SetCursorPosition(0, 5);
-                        _drawer.DeleteLine(5);
-                        _drawer.Write("1000 tap :o");
-                        break;
-                    default:
-                        break;
-                }
-                _drawer.Cursor_StandBy();
-            };
-
-            // After 1000ttt, the player can
-            // request the UI
-            Console.Write("réclamer quelque-chose contre {0} ttt ? (O/N)", Stats.Player.TaptaptapScore);
-            bool YES = false;
-            int emoji = 0;
-
-            while (!YES)
-            {
-                Console.SetCursorPosition(0, 6);
-                switch (Console.ReadKey().Key)
-                {
-                    case ConsoleKey.O:
-                        YES = true;
-                        _drawer.DeleteLine(6);
-                        _drawer.DeleteLine(5);
-                        _drawer.DeleteLine(Console.WindowHeight - 2);
-                        Console.SetCursorPosition(0, 5);
-                        Console.Write("Conversion !");
-                        break;
-                    case ConsoleKey.N:
-                        _drawer.DeleteLine(6);
-                        switch (emoji)
-                        {
-                            case 0:
-                                Console.Write(".-.");
-                                emoji++;
-                                break;
-                            case 1:
-                                Console.Write(".o.");
-                                emoji++;
-                                break;
-                            case 2:
-                                Console.Write("._.");
-                                emoji++;
-                                break;
-                            case 3:
-                                Console.Write(".__.");
-                                emoji++;
-                                break;
-                            case 4:
-                                Console.Write(".___.");
-                                emoji++;
-                                break;
-                            case 5:
-                                Console.Write("-___-");
-                                emoji++;
-                                break;
-                            case 6:
-                                Console.Write("-.-");
-                                emoji = 0;
-                                break;
-                            default:
-                                Console.Write("._.");
-                                break;
-                        }
-                        break;
-                    default:
-                        _drawer.DeleteLine(6);
-                        Console.Write("Vous n'avez pas choisi :/");
-                        break;
-                }
-                Console.CursorVisible = false;
-            }
-            Console.ReadKey();
-            Console.Clear();
-        }
 
         #region actions
         public void ActionChoice()
---
+++ jeu/jeu/Taptaptap.cs
@@ -0,0 +1,154 @@
+﻿using game;
+using Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeu
+{
+    /**
+    * mini-game at the beginning of the party
+    * 
+    * Allow the player to get the UI
+    */
+    class Taptaptap
+    {
+        GraphicTools _drawer;
+
+        /**
+         * Taptaptap is a mini-game.
+         * 
+         * Auto-launching at instanciation
+         */
+        public Taptaptap()
+        {
+            _drawer = new GraphicTools();
+            if (Stats.Player.TaptaptapScore < 1000)
+            {
+                StartingAnimation();
+                Play();
+            }
+            MenuBar.Display();
+        }
+
+        private void Play()
+        {
+            // Mini-Game loop
+            // finish with a score of 1K
+            while (Stats.Player.TaptaptapScore < 1000)
+            {
+                _drawer.DeleteLine(3);
+                _drawer.Cursor_StandBy();
+                Console.ReadKey();
+                Stats.Player.TaptaptapScore++;
+                Console.SetCursorPosition(Console.WindowWidth - 10, Console.WindowHeight - 2);
+                _drawer.Write("ttt: " + Stats.Player.TaptaptapScore);
+
+                switch (Stats.Player.TaptaptapScore)
+                {
+                    case 10:
+                        _drawer.DeleteLine(5);
+                        Console.SetCursorPosition(0, 5);
+                        _drawer.Write("Woaw 10 tap !");
+                        break;
+                    case 50:
+                        _drawer.DeleteLine(5);
+                        Console.SetCursorPosition(0, 5);
+                        _drawer.Write("50 tap !");
+                        break;
+                    case 100:
+                        _drawer.DeleteLine(5);
+                        Console.SetCursorPosition(0, 5);
+                        _drawer.Write("100 tap !!");
+                        break;
+                    case 200:
+                        _drawer.DeleteLine(5);
+                        Console.SetCursorPosition(0, 5);
+                        _drawer.Write("100 tap !!");
+                        break;
+                    case 500:
+                        _drawer.DeleteLine(5);
+                        Console.SetCursorPosition(0, 5);
+                        _drawer.Write("500 tap !!!! presque :)");
+                        break;
+                    case 1000:
+                        _drawer.DeleteLine(5);
+                        Console.SetCursorPosition(0, 5);
+                        _drawer.Write("1000 tap :o");
+                        break;
+                    default:
+                        break;
+                }
+                _drawer.Cursor_StandBy();
+            };
+
+
+            // After 1000ttt, the player can
+            // request the UI
+            _drawer.DeleteLine(5);
+            Console.SetCursorPosition(0, 5);
+            Console.Write("réclamer quelque-chose contre {0} ttt ? (O/N)", Stats.Player.TaptaptapScore);
+            bool YES = false;
+            Random emojiID = new Random();
+
+            while (!YES)
+            {
+                _drawer.Cursor_StandBy();
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.O:
+                        YES = true;
+                        _drawer.DeleteLine(6);
+                        _drawer.DeleteLine(5);
+                        _drawer.DeleteLine(Console.WindowHeight - 2);
+                        Console.SetCursorPosition(0, 5);
+                        Console.Write("Conversion !");
+                        break;
+                    case ConsoleKey.N:
+                        _drawer.DeleteLine(6);
+                        Console.SetCursorPosition(0, 6);
+                        string[] emojis = { ".-.", ".o.", "._.", ".__.", ".___.", "-___-", "-.-", "._." };
+                        int emoji = emojiID.Next(emojis.Length);
+                        Console.Write(emojis[emoji]);
+                        break;
+                    default:
+                        _drawer.DeleteLine(6);
+                        Console.SetCursorPosition(0, 6);
+                        Console.Write("Vous n'avez pas choisi :/");
+                        break;
+                }
+            }
+            Console.Clear();
+        }
+
+        private void StartingAnimation()
+        {
+            Console.Clear();
+            _drawer.HorizontalLine(0, '=');
+            Console.SetCursorPosition(0, 2);
+            ConsoleColor fg_actuel = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
+            _drawer.CenterWrite("tap tap tap !");
+            Console.ForegroundColor = fg_actuel;
+            _drawer.HorizontalLine(4, '=');
+            Console.CursorVisible = false;
+
+            // display the player sprite fall
+            for (int i = Console.CursorTop + 1; i < Console.WindowHeight; i++)
+            {
+                _drawer.CenterWrite(Sprite_box._player_base);
+                // the duration is 1 sec
+                // so each line last a division by
+                // the number of lines
+                _drawer.Wait(1000 / Console.WindowHeight);
+                Console.Write("\r");
+                _drawer.CenterWrite("   ");
+                Console.Write("\n");
+            }
+            _drawer.CenterWrite(Sprite_box._player_base);
+            Console.CursorVisible = true;
+        }
+    }
+}
